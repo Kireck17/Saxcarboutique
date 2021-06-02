e7dae2465e936c3231462ded1b575a8ca9094722
Orlando Ivan Basulto Benitez <gouiterra@gmail.com>
2021-06-03 00:50:46 +0200

    Agregado con toda la grasa

--- resources/views/components/containers/main.blade.php
+++ resources/views/components/containers/main.blade.php
@@ -1,3 +1,3 @@
-<div {{$attributes->merge(['class' =>'mx-w-7xl mx-auto sm:px-4 space-y-6 py-8'])}}>
+<div {{$attributes->merge(['class' =>'mx-w-7xl mx-auto overflow-hidden sm:px-4 space-y-6 py-8'])}}>
         {{$slot}}
 </div>
